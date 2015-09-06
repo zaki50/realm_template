98d1c16aae6779ba18f0fd282cec1435be15ece9
Makoto Yamazaki <makoto1975@gmail.com>
2015-09-07 01:06:29 +0900

    add JVM test which uses PowerMock and robolectric

--- app/src/test/java/com/example/realm_template/ExampleUnitTest.java
+++ app/src/test/java/com/example/realm_template/ExampleUnitTest.java
@@ -1,15 +1,43 @@
 package com.example.realm_template;
 
+import android.os.SystemClock;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.*;
+import io.realm.Realm;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+@PowerMockIgnore({"org.mockito.*", "org.robolectric.*", "android.*"})
+@PrepareForTest({Realm.class})
 public class ExampleUnitTest {
+    // Robolectric, Using Power Mock https://github.com/robolectric/robolectric/wiki/Using-PowerMock
+
+    @Rule
+    public PowerMockRule rule = new PowerMockRule();
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void mockRealm() throws Exception {
+        final Realm mockRealm = PowerMockito.mock(Realm.class);
+        PowerMockito.when(mockRealm.isAutoRefresh()).thenReturn(true);
+
+        assertThat(mockRealm.isAutoRefresh(), is(true));
+    }
+
+    @Test
+    public void mockRobolectric() throws Exception {
+        assertThat(SystemClock.uptimeMillis(), is(equalTo(100L)));
     }
 }
