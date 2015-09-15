50fd4fb21e100b76419016d2c2920acc2b4f6727
Makoto Yamazaki <makoto1975@gmail.com>
2015-09-16 07:02:04 +0900

    disable Leak Canary and Stetho on JVM UnitTest

--- app/src/debug/java/com/example/realm_template/DebugApplication.java
+++ app/src/debug/java/com/example/realm_template/DebugApplication.java
@@ -1,5 +1,7 @@
 package com.example.realm_template;
 
+import android.content.pm.PackageManager;
+
 import com.facebook.stetho.Stetho;
 import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
@@ -8,14 +10,20 @@ public class DebugApplication extends MyApplication {
     public void onCreate() {
         super.onCreate();
 
-        Stetho.initialize(
-                Stetho.newInitializerBuilder(this)
-                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this)
-                                .withMetaTables()
-                                .withDescendingOrder()
-                                .withLimit(1000)
-                                .build())
-                        .build());
+        if (!isOnRobolectricTest()) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(RealmInspectorModulesProvider.builder(this)
+                                    .withMetaTables()
+                                    .withDescendingOrder()
+                                    .withLimit(1000)
+                                    .build())
+                            .build());
+        }
+    }
+
+    private boolean isOnRobolectricTest() {
+        return getPackageManager().getClass().getName().startsWith("org.robolectric.");
     }
 }
